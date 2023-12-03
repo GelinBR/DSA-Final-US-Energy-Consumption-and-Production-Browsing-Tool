ab7efc89cda73a028f4cfcdb72c69f03df57d78f
SamiSepehri <101594473+SamiSepehri@users.noreply.github.com>
2023-12-03 01:00:52 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,50 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <map>
 #include <vector>
+#include <algorithm>
 #include "EnergyDataPoint.h"
 #include "EnergyData.h"
 using namespace std;
+
+struct EnergyType {
+    std::string type;
+    std::vector<std::string> methods;
+};
+
+std::map<std::string, EnergyType> energyData = {
+    {"Commercial", {"Commercial", {"Coal", "Distillate Fuel Oil", "Geothermal", "Hydropower", "Kerosene", "Petroleum", "Natural Gas", "Solar", "Wind", "Wood"}}},
+    {"Electric Power", {"Electric Power", {"Coal", "Distillate Fuel Oil", "Natural Gas", "Wood"}}},
+    {"Industrial", {"Industrial", {"Coal", "Distillate Fuel Oil", "Geothermal", "Hydropower", "Kerosene", "Petroleum", "Natural Gas", "Other Petroleum Products","Solar", "Wind", "Wood"}}},
+    {"Refinery", {"Refinery", {"Coal", "Distillate Fuel Oil", "Natural Gas"}}},
+    {"Residential", {"Residential", {"Coal", "Distillate Fuel Oil", "Geothermal", "Kerosene", "Petroleum", "Natural Gas", "Wood"}}},
+    {"Transportation", {"Transportation", {"Coal", "Distillate Fuel Oil", "Petroleum", "Natural Gas"}}},
+};
+
+void displayOptions() {
+    std::cout << "Consumption methods:\n";
+    for (const auto& entry : energyData) {
+        std::cout << entry.first << "\n";
+    }
+}
+
+void displayMethods(const std::string& type) {
+    const auto& methods = energyData[type].methods;
+    std::cout << "Energy types for " << type << ":\n";
+    for (const auto& method : methods) {
+        std::cout << method << "\n";
+    }
+}
+
+bool isValidType(const std::string& type) {
+    return energyData.find(type) != energyData.end();
+}
+
+bool isValidMethod(const std::string& type, const std::string& method) {
+    const auto& methods = energyData[type].methods;
+    return std::find(methods.begin(), methods.end(), method) != methods.end();
+}
 
 int main() {
     // File selection
@@ -20,6 +60,43 @@ int main() {
     // Dataset Creation
     EnergyData dataset = EnergyData(filename);
     //dataset.print();
+
+    std::string energyType, consumptionMethod;
+
+    //Welcome message
+    std::cout << "Welcome to the US Energy Consumption History Browsing Tool!" << std::endl;
+
+    // Display options
+    displayOptions();
+
+    while (true) {
+        std::cout << "Enter consumption method: ";
+        std::cin >> consumptionMethod;
+
+        if (isValidType(consumptionMethod)) {
+            break;
+        } else {
+            std::cout << "Invalid consumption method. Please try again.\n";
+        }
+    }
+
+    // Display valid consumption methods for the selected energy type
+    displayMethods(consumptionMethod);
+
+    // Get and validate user input for consumption method
+    while (true) {
+        std::cout << "Enter energy type: ";
+        std::cin >> energyType;
+
+        if (isValidMethod(consumptionMethod, energyType)) {
+            break;
+        } else {
+            std::cout << "Invalid energy type. Please try again.\n";
+        }
+    }
+
+    //concatenate Consumption.energyType.consumptionMethod
+    //call each sortings function(quick sort and Merge sort)
 
 	return 0;
 }
